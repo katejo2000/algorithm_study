4a62fc33276cb6e01839c4cfbffff861edee15e6
Kyung Hyun Jo <53073097+katejo2000@users.noreply.github.com>
2024-05-03 15:02:11 +0900

    Update 2875.py

--- greedy/2875.py
+++ greedy/2875.py
@@ -8,7 +8,6 @@ girl, boy, k = map(int, input().split())
 team = 0
 
 while girl >= 2 and boy >= 1 and girl + boy >= k + 3:
-    print(girl, boy, girl + boy, k)
     girl -= 2
     boy -= 1
     team += 1
@@ -18,5 +17,4 @@ while girl >= 2 and boy >= 1 and girl + boy >= k + 3:
 #     boy += 1
 #     team -= 1
 
-print("final :", girl, boy, girl + boy, k)
 print(team)
